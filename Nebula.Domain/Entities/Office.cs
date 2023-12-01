ebef12eb206dffff44e6cac3c8e2329cc8d96b29
jakhangir-esanov <alibaba.esanov@gmail.com>
2023-12-01 12:18:48 +0300

    added unit test

--- Nebula.Domain/Entities/Office.cs
+++ Nebula.Domain/Entities/Office.cs
@@ -5,5 +5,13 @@ namespace Nebula.Domain.Entities;
 public sealed class Office : Auditable
 {
     public string Name { get; set; }
-
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State {  get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Website { get; set; }
+    public string Description { get; set; }
 }
