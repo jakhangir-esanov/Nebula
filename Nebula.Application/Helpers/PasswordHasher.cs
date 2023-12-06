c169b478817a28e3882a256d7f1204e40823b789
jakhangir-esanov <alibaba.esanov@gmail.com>
2023-12-06 10:11:22 +0300

    Added Password hasher

---
+++ Nebula.Application/Helpers/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿namespace Nebula.Application.Helpers;
+
+public static class PasswordHasher
+{
+    public static (string Password, string Salt) Hash(string password)
+    {
+        string salt = Guid.NewGuid().ToString();
+        string hash = BCrypt.Net.BCrypt.HashPassword(password + salt);
+        return (Password: hash, Salt: salt);
+    }
+
+    public static bool Verify(string password, string hasPassword, string salt)
+    {
+        return BCrypt.Net.BCrypt.Verify(password + salt, hasPassword);
+    }
+}
