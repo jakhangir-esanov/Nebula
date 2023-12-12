a2246979842a0659aedcafaace755ade2359d45c
jakhangir-esanov <alibaba.esanov@gmail.com>
2023-12-12 09:23:47 +0300

    Controller Test added

---
+++ Nebula.UnitTest/Controller/Car/CarCategoryControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Nebula.Application.Commands.Cars.CreateCarCategory;
+using Nebula.WebApi.Controllers;
+
+namespace Nebula.UnitTest.Controller.Car;
+
+public class CarCategoryControllerTest
+{
+    private Mock<IMediator> mediatr;
+    private CarCategoryController controller;
+    public CarCategoryControllerTest()
+    {
+        mediatr = new Mock<IMediator>();
+        controller = new CarCategoryController(mediatr.Object);
+    }
+
+    [Fact]
+    public async Task AddCarCategorySuccessfully()
+    {
+        //Arrange 
+        var carCategory = new CreateCarCategoryCommand("name", 25, "description", 250);
+
+        //Act
+        var result = await controller.CreateAsync(carCategory);
+
+        //Asseert
+        Assert.NotNull(result);
+        Assert.IsType<OkObjectResult>(result);
+    }
+}
--- Nebula.UnitTest/Controller/Car/CarCommandTest.cs
+++ Nebula.UnitTest/Controller/Car/CarCommandTest.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nebula.UnitTest.Queries.Insurance
+namespace Nebula.UnitTest.Controller.Car
 {
-    internal class InsuranceQueryTest
+    internal class CarCommandTest
     {
     }
 }
